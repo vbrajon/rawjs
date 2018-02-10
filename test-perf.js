cda8338444faf4f5bf029e434beee2fc2317272e
Valentin Brajon <vbrajon@gmail.com>
2018-02-10 11:18:25 +0100

    test: loop + output

--- test-perf.js
+++ test-perf.js
@@ -1,60 +1,44 @@
 const Benchmark = require('benchmark')
-const suite = new Benchmark.Suite
 
 const _ = require('lodash')
 const Sugar = require('Sugar')
 const xtend = require('./xtend')
 
-const obj = { a: { b: [1, 2, 3] }, c: null }
-const fn = v => v && v.b && v.b[1]
-const reduce_fn = (acc, v, k) =>  {
-  acc.push(v && v.b && v.b[1] || k)
-  return acc
-}
+;['map', 'filter', 'find'].map(fname => {
+  const suite = new Benchmark.Suite
 
-console.log('map')
-console.log(_.map(obj, fn))
-console.log(Sugar.Object.map(obj, fn))
-console.log(xtend.Object.map(obj, fn))
+  const obj = { a: { b: [1, 2, 3] }, c: null }
+  const fn = v => v && v.b && v.b[1]
+  const reduce_fn = (acc, v, k) =>  {
+    acc.push(v && v.b && v.b[1] || k)
+    return acc
+  }
+  const args = ({
+    map: [obj, fn],
+    reduce: [obj, reduce_fn, []],
+    filter: [obj, fn],
+    find: [obj, fn],
+  })[fname]
+  const stringify = arg => typeof arg === 'function' ? arg.toString() : JSON.stringify(arg)
 
-console.log('filter')
-console.log(_.filter(obj, fn))
-console.log(Sugar.Object.filter(obj, fn))
-console.log(xtend.Object.filter(obj, fn))
+  // console.log('params: (' + args.map(stringify).join(', ') + ')')
+  ;['sugar', 'lodash', 'xtend'].map(libname => {
+    const libfn = ({
+      lodash: _,
+      sugar: Sugar.Object,
+      xtend: xtend.Object,
+    })[libname][fname]
 
-console.log('find')
-console.log(_.find(obj, fn))
-console.log(Sugar.Object.find(obj, fn))
-console.log(xtend.Object.find(obj, fn))
+    // console.log(libname + '#' + fname, libfn(...args))
+    suite.add(libname + '#' + fname, () => libfn(...args))
+  })
 
-console.log('reduce')
-console.log(_.reduce(obj, reduce_fn, []))
-console.log(Sugar.Object.reduce(obj, reduce_fn, []))
-console.log(xtend.Object.reduce(obj, reduce_fn, []))
-
-suite.add('lodash', function() {
-  _.map(obj, fn)
-  _.filter(obj, fn)
-  _.find(obj, fn)
-  _.reduce(obj, reduce_fn, [])
+  // green: '\x1b[32m', yellow: '\x1b[33m', reset: '\x1b[0m'
+  const start = txt => console.log('\x1b[32m', txt, '\x1b[0m')
+  const complete = txt => console.log('\x1b[33m', txt, '\x1b[0m', '\n')
+  suite
+    .on('start', ev => start('Starting "' + fname + '" Test Suite'))
+    .on('cycle', ev => console.log(String(ev.target)))
+    .on('complete', () => complete('The fastest is ' + suite.filter('fastest').map('name')))
+    .run()
 })
-
-suite.add('sugar', function() {
-  Sugar.Object.map(obj, fn)
-  Sugar.Object.filter(obj, fn)
-  Sugar.Object.find(obj, fn)
-  Sugar.Object.reduce(obj, reduce_fn, [])
-})
-
-suite.add('xtend', function() {
-  xtend.Object.map(obj, fn)
-  xtend.Object.filter(obj, fn)
-  xtend.Object.find(obj, fn)
-  xtend.Object.reduce(obj, reduce_fn, [])
-})
-
-
-suite
-  .on('cycle', ev => console.log(String(ev.target)))
-  .on('complete', () => console.log('The fastest is ' + suite.filter('fastest').map('name')))
-  .run({ async: true })
