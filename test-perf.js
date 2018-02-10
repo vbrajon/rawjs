740cd0570ec6bda9b4bf718945c280e41ad7a18c
Valentin Brajon <vbrajon@gmail.com>
2018-02-10 21:41:03 +0100

    test: improve test output

--- test-perf.js
+++ test-perf.js
@@ -4,41 +4,89 @@ const _ = require('lodash')
 const Sugar = require('Sugar')
 const xtend = require('./xtend')
 
-;['map', 'filter', 'find'].map(fname => {
-  const suite = new Benchmark.Suite
+const n = 10000
+const a = []
+const o = {}
+for(let i = 0; i < n; ++i) {
+  a[i] = Math.random() * 2 - 1
+  o[String.fromCodePoint(i)] = Math.random() * 2 - 1
+}
 
-  const obj = { a: { b: [1, 2, 3] }, c: null }
-  const fn = v => v && v.b && v.b[1]
-  const reduce_fn = (acc, v, k) =>  {
-    acc.push(v && v.b && v.b[1] || k)
-    return acc
-  }
-  const args = ({
-    map: [obj, fn],
-    reduce: [obj, reduce_fn, []],
-    filter: [obj, fn],
-    find: [obj, fn],
-  })[fname]
-  const stringify = arg => typeof arg === 'function' ? arg.toString() : JSON.stringify(arg)
-
-  // console.log('params: (' + args.map(stringify).join(', ') + ')')
-  ;['sugar', 'lodash', 'xtend'].map(libname => {
+;['map', 'reduce', 'filter', 'find'].map(fname => {
+  const suite = new Benchmark.Suite('#' + fname)
+  ;['xtend', 'sugar', 'lodash'].map(libname => {
+    // Arguments
+    const map_fn = v => v
+    const reduce_fn = (acc, v, k) =>  { acc.push(v);return acc }
+    const args = ({
+      map: [o, map_fn],
+      reduce: [o, reduce_fn, []],
+      filter: [o, map_fn],
+      find: [o, map_fn],
+    })[fname]
     const libfn = ({
       lodash: _,
       sugar: Sugar.Object,
       xtend: xtend.Object,
     })[libname][fname]
 
-    // console.log(libname + '#' + fname, libfn(...args))
-    suite.add(libname + '#' + fname, () => libfn(...args))
+    // Input & Output for the current Test Run
+    const stringify = arg => {
+      const type = Object.prototype.toString.call(arg).slice(8, -1).toLowerCase()
+      if (type === 'function') return arg.toString()
+      if (type === 'array') return '[ ' + stringify(arg[0]) + ' <' + arg.length + '> ]'
+      if (type === 'object') return '{ ' + Object.keys(arg)[0] + ': ' + stringify(Object.values(arg)[0]) + ' <' + Object.keys(arg).length + '> }'
+      if (type === 'string') return arg.length > 3 ? arg.slice(0, 3) + '...' : arg
+      if (type === 'number') return arg.toPrecision(2)
+    }
+    const current = libname + '#' + fname
+    const input = '(' + args.map(stringify).join(', ') + ')'
+    const output = stringify(libfn(...args))
+    console.log(current, input, '>', output)
+    suite.add(libname, () => libfn(...args))
   })
 
-  // green: '\x1b[32m', yellow: '\x1b[33m', reset: '\x1b[0m'
-  const start = txt => console.log('\x1b[32m', txt, '\x1b[0m')
-  const complete = txt => console.log('\x1b[33m', txt, '\x1b[0m', '\n')
   suite
-    .on('start', ev => start('Starting "' + fname + '" Test Suite'))
-    .on('cycle', ev => console.log(String(ev.target)))
-    .on('complete', () => complete('The fastest is ' + suite.filter('fastest').map('name')))
+    .on('start', logStart)
+    .on('cycle', logResults)
+    .on('complete', logComplete)
     .run()
 })
+
+function logResults(e) {
+  var t = e.target;
+
+  if(t.failure) {
+    console.error(padl(10, t.name) + 'FAILED: ' + e.target.failure);
+  } else {
+    var result = padl(10, t.name)
+      + padr(13, t.hz.toFixed(2) + ' op/s')
+      + ' \xb1' + padr(7, t.stats.rme.toFixed(2) + '%')
+      + padr(15, ' (' + t.stats.sample.length + ' samples)');
+
+    console.log(result);
+  }
+}
+
+function logStart() {
+  console.log(this.name);
+  console.log('-----------------------------------------------');
+}
+
+function logComplete() {
+  console.log('-----------------------------------------------');
+}
+
+function padl(n, s) {
+  while(s.length < n) {
+    s += ' ';
+  }
+  return s;
+}
+
+function padr(n, s) {
+  while (s.length < n) {
+    s = ' ' + s;
+  }
+  return s;
+}
